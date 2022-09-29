89277b7e4f5cc3d5567d1240ba4c05d73432ab0b
suhejb-jpg <78593111+suhejb-jpg@users.noreply.github.com>
2022-09-29 23:42:57 +0200

    Add files via upload

---
+++ anagram/anagram.js
@@ -0,0 +1,20 @@
+var input = "linkit, itlink, suhejb, bjehus, subjeh, shtepi, ipetsh, anagram, margana";
+var words = input.split(", ");
+
+for (var i = 0; i < words.length; i++) {
+
+  var word = words[i];
+  var alphabetical = word.split("").sort().join("");
+
+  for (var j = 0; j < words.length; j++) {
+
+    if (i === j) {
+      continue;
+    }
+
+    var other = words[j];
+    if (alphabetical === other.split("").sort().join("")) {
+      console.log(word + " - " + other + " (" + i + ", " + j + ")");
+    }
+  }
+}
